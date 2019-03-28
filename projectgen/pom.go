5ecdd3ada3044886fae54eebd1e93f8505c3c6c4
Davide Berdin <davideberdin@gmail.com>
2019-03-28 22:25:08 +0100

    removed projectgen package

--- projectgen/pom.go
+++
@@ -1,6 +0,0 @@
-package projectgen
-
-// GeneratePomXML will return the pom.xml file
-func GeneratePomXML() string {
-	return ""
-}
--- projectgen/project.go
+++
@@ -1,5 +0,0 @@
-package projectgen
-
-func GenerateProject() {
-
-}
